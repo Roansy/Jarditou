24089904ce6efe0bfbf201ec73df724ea0da54c7
Roansy <romainbrege@gmail.com>
2023-10-18 15:18:57 +0200

    exe

---
+++ script.js
@@ -0,0 +1,18 @@
+alert("coucou");
+console.log("méthode log");
+var nom = prompt("nom");
+alert(nom)
+var prenom = prompt("prenom");
+alert(prenom)
+confirm ("oui");
+
+function convert(){
+    var c = document.getElementById('data').value;
+    var f = (c*9/5 ) + 32;
+    alert(f);
+}
+
+
+
+
+
